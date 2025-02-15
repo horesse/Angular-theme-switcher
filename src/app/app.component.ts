99c262210d307a933b6047a3609da58827531bc8
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-02-15 11:52:39 +0300

    Добавил encapsulation для app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-  imports: [
-    RouterOutlet
-  ],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+    encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
     title = 'Angular-theme-switcher';
